b5b29cb621bfbd7e745062a7ecc30659334a245e
ratadune <ratadune@gmail.com>
2024-09-18 20:28:02 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.BASE_PATH || "",
-  basePath: process.env.BASE_PATH || "/reitaiwan",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   trailingSlash: true,
   publicRuntimeConfig: {
-    root: process.env.BASE_PATH || "/reitaiwan",
+    root: process.env.NEXT_PUBLIC_BASE_PATH || "",
   },
   optimizeFonts: false,
 };
